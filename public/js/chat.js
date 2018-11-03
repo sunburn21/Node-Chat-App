874fdc7f585999252f7cb1cdf2c02e6d6aefba72
sunburn21 <36687316+sunburn21@users.noreply.github.com>
2018-11-03 11:22:43 +0530

    Sending msgs to privately

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 var socket = io();
-// var deparam = require('./libs/deparam')
-function scrollToBottom() {
+
+function scrollToBottom () {
   // Selectors
   var messages = jQuery('#messages');
   var newMessage = messages.children('li:last-child')
@@ -18,23 +18,30 @@ function scrollToBottom() {
 
 socket.on('connect', function () {
   var params = jQuery.deparam(window.location.search);
+
   socket.emit('join', params, function (err) {
     if (err) {
       alert(err);
-
+      window.location.href = '/';
     } else {
-      console.log('No error')
+      console.log('No error');
     }
-  })
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('updateUsersList', function (users) {
-  console.log(users);
-})
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -68,7 +75,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
--- server/utils/users.js
+++ server/utils/users.js
@@ -1,26 +1,55 @@
+[{
+  id: '/#12poiajdspfoif',
+  name: 'Andrew',
+  room: 'The Office Fans'
+}]
+
+// addUser(id, name, room)
+// removeUser(id)
+// getUser(id)
+// getUserList(room)
+
 class Users {
-    constructor() {
-        this.users = []
-    }
-    removeUser(id) {
-        const user = this.getUser(id);
-        if (user) {
-            this.users = this.users.filter(doc => doc.id !== id);
-        }
-        return user;
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
     }
 
-    getUser(id) {
-        return this.users.filter((user) => (user.id === id))[0];
-    }
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0]
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
 
-    addUser(id, name, room) {
-        this.users.push({ id, name, room })
-    }
-
-    getUsersList(room) {
-        return this.users.filter((user) => (user.room === room)).map((user) => (user.name));
-    }
+    return namesArray;
+  }
 }
 
-module.exports.Users = Users;
+module.exports = {Users};
+
+ // class Person {
+ //   constructor (name, age) {
+ //     this.name = name;
+ //     this.age = age;
+ //   }
+ //   getUserDescription () {
+ //     return `${this.name} is ${this.age} year(s) old.`;
+ //   }
+ // }
+ //
+ // var me = new Person('Andrew', 25);
+ // var description = me.getUserDescription();
+ // console.log(description);
